c722a461cd5cbd16a0744f407e74ade5c1f5cd29
Ruslan <luhoviy.ruslan@gmail.com>
2021-12-11 00:37:23 +0200

    minor improvements

--- src/app/store/articles/articles.effects.ts
+++ src/app/store/articles/articles.effects.ts
@@ -2,12 +2,12 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
 import { delay, map, withLatestFrom } from "rxjs";
-import * as fromFeature from ".";
 import { tap } from "rxjs/operators";
 import { updateLoadingState } from "../app.actions";
 import { NotificationsService } from "../../shared/services/notifications.service";
 import { getCurrentUser } from "../../authentication/store";
 import { cloneDeep } from "lodash";
+import { createArticle, createArticleSuccess } from "./articles.actions";
 
 @Injectable()
 export class ArticlesEffects {
@@ -19,17 +19,17 @@ export class ArticlesEffects {
 
   createArticle$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(fromFeature.createArticle),
+      ofType(createArticle),
       withLatestFrom(this.store.select(getCurrentUser)),
-      tap(() => this.store.dispatch(updateLoadingState({ isLoading: true }))),
+      tap(() => this.store.dispatch(updateLoadingState({isLoading: true}))),
       delay(500),
-      map(([{ article }, user]) => {
+      map(([{article}, user]) => {
         article = cloneDeep(article);
         article.authorId = user?.id;
         article.author = cloneDeep(user);
-        this.store.dispatch(updateLoadingState({ isLoading: false }));
+        this.store.dispatch(updateLoadingState({isLoading: false}));
         this.notifications.success("Article has been successfully published!");
-        return fromFeature.createArticleSuccess({ article });
+        return createArticleSuccess({article});
       })
     )
   );
